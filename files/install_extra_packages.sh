4e55a1041d732749ac73bb56fe64dc6afac022ba
Kevin Durbin <kevindurb@users.noreply.github.com>
2024-12-29 06:47:52 -0700

    Update install_extra_packages.sh

--- files/install_extra_packages.sh
+++ files/install_extra_packages.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 set -eux -o pipefail
 
-sudo dnf copr enable atim/lazygit -y
-sudo dnf copr enable atim/starship -y
+dnf copr enable atim/lazygit -y
+dnf copr enable atim/starship -y
 
 dnf update -y
 grep -v '^#' /tmp/extra-packages | xargs dnf install -y
