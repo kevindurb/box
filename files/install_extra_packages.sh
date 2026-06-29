12b2d5c38a5fe09ac3494aaaff6a4ab3c0e57f2f
Kevin Durbin <kevindurb@fastmail.com>
2026-06-29 09:03:08 -0600

    reset back to vanilla to start over

--- files/install_extra_packages.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -eux -o pipefail
-
-dnf copr enable atim/lazygit -y
-dnf copr enable atim/starship -y
-
-dnf update -y
-grep -v '^#' /tmp/extra-packages | xargs dnf install -y
-dnf clean all
